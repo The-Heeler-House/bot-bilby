ad2f0fa90b0e3c78399b75b4e149d15311278d5c
Jalen Lu <jalenlu2000@icloud.com>
2023-08-03 08:14:39 +0000

    oops forgo changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -12,10 +12,10 @@ module.exports = {
         {
           "title": "Bot Changelog",
           "color": 9356018,
-          "description": "- New episode guesser feature!.\n- New multiplayer episode guesser!\n- Revamped ship command, complete with a new character picker.\n- Deprecated the rarely used tiermaker commands.\n- Search Blueypedia inside of Discord with the new Character command!\n- More commands coming soon!",
-          "timestamp": "2022-01-03",
+          "description": "- BILBY MUSIC PLAYER!!!\n - New episode guesser feature!.\n- New multiplayer episode guesser!\n- Revamped ship command, complete with a new character picker.\n- Search Blueypedia inside of Discord with the new Character command!\n- More commands coming soon!",
+          "timestamp": "2023-07-03",
           "author": {
-            "name": "Bot Bilby 2.3.1",
+            "name": "Bot Bilby 2.5.0",
             "url": "",
             "icon_url": "https://cdn.discordapp.com/avatars/537583059348750336/22e5087eb405782afccab3e635c7df91.png?size=64"
           },
