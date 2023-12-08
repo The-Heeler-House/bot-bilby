0786a8208a3134adb175c6c90fc2c05da159190c
Jalen Lu <jalenlu2000@icloud.com>
2023-12-08 17:13:33 +0000

    Update Secret Santa registration message

--- commands/secretsanta.js
+++ commands/secretsanta.js
@@ -16,7 +16,7 @@ module.exports = {
 
         // Send a success message to the user via DM
         await interaction.reply({ content: `You are now registered for the Secret Santa event with the prompt: ${prompt}`, ephemeral: true });
-        await interaction.channel.send({ content: `Thank you for registering for the Secret Santa event!`});
+        await interaction.channel.send({ content: `Thank you for registering for the Secret Santa event! For other members, if you want to register, use the \`/secretsanta\` command.`});
         // Get the staff channel where the event details will be sent
         const staffChannel = interaction.guild.channels.cache.find(channel => channel.name === 'secretsanta-list');
 
