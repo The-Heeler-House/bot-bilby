92146497ee66339cf43e0b11d311cb6f90d615ad
Jalen Lu <jalenlu2000@icloud.com>
2024-01-04 01:25:50 -0700

    test?

--- commands/ping.js
+++ commands/ping.js
@@ -2,11 +2,11 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('is bilby dying'),
-async execute(interaction) {
-    await interaction.deferReply();
-    await interaction.editReply(`Websocket heartbeat: ${interaction.client.ws.ping}ms.`);
-}
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('is bilby dying'),
+    async execute(interaction) {
+        await interaction.deferReply();
+        await interaction.followUp(`Websocket heartbeat: ${interaction.client.ws.ping}ms.`);
+    }
 };
