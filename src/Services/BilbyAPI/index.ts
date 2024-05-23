3014165f11328b8df02ff0024ab8c97c14ea4cc8
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-05-23 20:47:00 +0100

    Cache API to avoid hammering Discord

--- src/Services/BilbyAPI/index.ts
+++ src/Services/BilbyAPI/index.ts
@@ -4,8 +4,12 @@ import * as logger from "../../Logger";
 
 export default class BilbyAPIService {
     protected client: Client;
-    protected guild: Guild;
     protected app: express.Express;
+
+    protected caching: Caching = { 
+        memberCount: { value: null, lastCacheTimestamp: 0 }, 
+        upcomingEvents: { value: [], lastCacheTimestamp: 0 }
+    };
 
     constructor(client: Client) {
         this.app = express();
@@ -13,9 +17,6 @@ export default class BilbyAPIService {
         client.on("ready", () => {
             this.client = client;
             
-            // DEVELOPMENT_GUILD is only set in a development environment, so by default we assume The Heeler House as target guild
-            // FIXME: Maybe this shouldn't be hardcoded? Will need a discussion regarding this.
-            this.client.guilds.fetch(process.env.DEVELOPMENT_GUILD ?? "959534476520730724").then(guild => this.guild = guild);
 
             this.app.get("/members", (req, res) => this.serverMembers(req, res));
             this.app.get("/events", (req, res) => this.serverEvents(req, res));
@@ -25,34 +26,62 @@ export default class BilbyAPIService {
     }
 
     async serverMembers(req: Request, res: Response) {
-        let allMembers = this.guild.members.cache;
-        let onlineMembers = allMembers.filter((member) => ["online", "idle", "dnd"].includes(member.presence?.status));
+        if (Date.now() - this.caching.memberCount.lastCacheTimestamp >= 1 * 60 * 1000) {
+            this.caching.memberCount.lastCacheTimestamp = Date.now()
 
-        res.status(200).send({
-            all: allMembers.size,
-            online: onlineMembers.size
-        });
+            // DEVELOPMENT_GUILD is only set in a development environment, so by default we assume The Heeler House as target guild
+            // FIXME: Maybe this shouldn't be hardcoded? Will need a discussion regarding this.
+            let guild = await this.client.guilds.fetch(process.env.DEVELOPMENT_GUILD ?? "959534476520730724");
+            let allMembers = guild.members.cache;
+            let onlineMembers = allMembers.filter((member) => ["online", "idle", "dnd"].includes(member.presence?.status));
+
+            this.caching.memberCount.value = {
+                all: allMembers.size,
+                online: onlineMembers.size
+            }
+        }
+
+        res.status(200).send(this.caching.memberCount.value);
     }
 
     async serverEvents(req: Request, res: Response) {
-        let events = await this.guild.scheduledEvents.fetch();
-        let upcomingEvents: UpcomingEvents[] = []
+        if (Date.now() - this.caching.upcomingEvents.lastCacheTimestamp >= 1 * 60 * 1000) {
+            this.caching.upcomingEvents.lastCacheTimestamp = Date.now()
 
-        events.forEach((event) => {
-            upcomingEvents.push({
-                name: event.name,
-                description: event.description,
-                ongoing: event.scheduledStartTimestamp <= Date.now(),
-                start: event.scheduledStartAt,
-                url: event.url,
-                image: event.coverImageURL()
+            // DEVELOPMENT_GUILD is only set in a development environment, so by default we assume The Heeler House as target guild
+            // FIXME: Maybe this shouldn't be hardcoded? Will need a discussion regarding this.
+            let guild = await this.client.guilds.fetch(process.env.DEVELOPMENT_GUILD ?? "959534476520730724");
+            let events = await guild.scheduledEvents.fetch();
+            let upcomingEvents: UpcomingEvents[] = []
+
+            events.forEach((event) => {
+                upcomingEvents.push({
+                    name: event.name,
+                    description: event.description,
+                    ongoing: event.scheduledStartTimestamp <= Date.now(),
+                    start: event.scheduledStartAt,
+                    url: event.url,
+                    image: event.coverImageURL()
+                });
             });
-        });
 
-        res.status(200).send(upcomingEvents.sort((event_a, event_b) => {
-            return event_a.start.getTime() - event_b.start.getTime();
-        }));
+            this.caching.upcomingEvents.value = upcomingEvents.sort((event_a, event_b) => {
+                return event_a.start.getTime() - event_b.start.getTime();
+            });
+        }
+
+        res.status(200).send(this.caching.upcomingEvents.value);
     }
+}
+
+interface Caching {
+    memberCount: { value: MemberCounts, lastCacheTimestamp: number},
+    upcomingEvents: { value: UpcomingEvents[], lastCacheTimestamp: number }
+}
+
+interface MemberCounts {
+    all: number,
+    online: number
 }
 
 interface UpcomingEvents {
