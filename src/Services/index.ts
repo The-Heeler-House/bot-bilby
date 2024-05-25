70f0e72d7a9703c0d1dbfb8405658686f93393e7
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-05-25 09:53:00 +0100

    Create analytics service for interfacing with rplace analytics api

--- src/Services/index.ts
+++ src/Services/index.ts
@@ -11,6 +11,7 @@ import CommandPreprocessor from "../Commands";
 import StateService from "./State";
 import BilbyAPIService from "./BilbyAPI";
 import PagerService from "./Pager";
+import AnalyticsService from "./Analytics";
 
 
 export default function getServices(client: Client, commands: CommandPreprocessor): Services {
@@ -22,7 +23,8 @@ export default function getServices(client: Client, commands: CommandPreprocesso
         commands,
         database: new DatabaseService(),
         state: new StateService(),
-        pager: new PagerService(client)
+        pager: new PagerService(client),
+        analytics: new AnalyticsService()
     }
 }
 
@@ -30,5 +32,6 @@ export interface Services {
     commands: CommandPreprocessor,
     database: DatabaseService,
     state: StateService,
-    pager: PagerService
+    pager: PagerService,
+    analytics: AnalyticsService
 }
