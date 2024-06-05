b01c80477f50a79e5e0d5cbb5c1cc1b0b5d2f365
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-06-05 11:27:21 +0100

    Fix server check

--- src/Helper/EventsHelper.ts
+++ src/Helper/EventsHelper.ts
@@ -1,7 +1,7 @@
 import { THH_SERVER_ID } from "../constants";
 
 export function isTHHorDevServer(guildId: string) {
-    return (process.env.DEVELOPMENT_GUILD ?? "") == "" ?
+    return (process.env.DEVELOPMENT_GUILD ?? "") != "" ?
         (guildId != process.env.DEVELOPMENT_GUILD) :
         (guildId != THH_SERVER_ID)
 }
