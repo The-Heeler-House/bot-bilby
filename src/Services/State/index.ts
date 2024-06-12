b5f55a7ea722c5c48d1ad9c0a507674e097da950
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-06-12 22:41:52 +0100

    Fix state service

--- src/Services/State/index.ts
+++ src/Services/State/index.ts
@@ -2,7 +2,7 @@ import { Message, Snowflake } from "discord.js";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import path from "path";
 
-const STATE_PATH = path.join(__dirname, "../../Assets/state.json");
+const STATE_PATH = path.join(__dirname, "../../../state.json");
 
 export default class StateService {
     public state: State;
