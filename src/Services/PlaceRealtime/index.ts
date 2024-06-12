9c895a5ac3a5d52dd2c83c47e8cc9854a1bfa2ed
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-06-12 18:55:59 +0100

    Unhardcode faction name, add error logging

--- src/Services/PlaceRealtime/index.ts
+++ src/Services/PlaceRealtime/index.ts
@@ -4,6 +4,7 @@ import * as logger from "../../logger";
 
 export default class PlaceRealtimeService extends EventEmitter {
     private client: mqtt.MqttClient;
+    public faction: string = "lemmy";
 
     constructor() {
         super();
@@ -23,22 +24,29 @@ export default class PlaceRealtimeService extends EventEmitter {
 
         this.client.on("connect", (packet) => {
             logger.command("R/Place Realtime connected.");
-            this.client.subscribe(`templates/bluey/#`);
+            this.client.subscribe(`templates/${this.faction}/#`);
             this.emit("ready");
         });
 
+        this.client.on("error", (error) => {
+            logger.error("Encountered an error from MQTT.\n", error.stack);
+        })
+
         this.client.on("message", (topicString: string, dataBuffer: Buffer, packet: mqtt.IPublishPacket) => {
             let topic = topicString.split("/");
+            try {
             let data = JSON.parse(dataBuffer.toString());
       
             this.emit(topic[2], data, packet.retain ?? false);
+            } catch (error) {
+                logger.error("Encountered an error while trying to emit MQTT message as an event. See error below.\n", error.stack);
+            }
         });
     }
 
     async publish(topic: string, data: any) {
-        return this.client.publishAsync(`templates/${process.env.REALTIME_FACTION}/${topic}`, Buffer.from(JSON.stringify(data)))
+        return this.client.publishAsync(`templates/${this.faction}/${topic}`, Buffer.from(JSON.stringify(data)))
         .catch((error: any) => {
-          console.error(error);
           throw error;
         });
     }
