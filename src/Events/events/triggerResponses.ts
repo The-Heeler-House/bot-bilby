b5a5152939cc640acf733dfc9d2f4bdfb464f500
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-07-04 18:28:17 +0100

    Fix trigger cooldown breaking with new DB ID handling
    A previous commit changed how we handle IDs in the database, this fixes an oversight made when doing this change which meant trigger cooldown stopped working.

--- src/Events/events/triggerResponses.ts
+++ src/Events/events/triggerResponses.ts
@@ -8,7 +8,7 @@ import { processResponse } from "../../Helper/DirectiveHelper";
 
 export default class TriggerResponseEvent extends BotEvent {
     public eventName = Events.MessageCreate;
-    private lastTriggered: Map<ObjectId, number> = new Map();
+    private lastTriggered: Map<string, number> = new Map();
 
     async execute(services: Services, message: Message) {
         if (!isTHHorDevServer(message.guild.id)) return;
@@ -17,17 +17,16 @@ export default class TriggerResponseEvent extends BotEvent {
 
         let triggers = await services.database.collections.triggers.find().toArray() as WithId<Trigger>[];
 
-        let triggerCanidates = triggers.filter(trigger => this.lastTriggered.has(trigger._id) ? Date.now() - this.lastTriggered.get(trigger._id) > (trigger.cooldown * 1000) : true)
+        let triggerCanidates = triggers.filter(trigger => this.lastTriggered.has(trigger._id.toHexString()) ? Date.now() - this.lastTriggered.get(trigger._id.toHexString()) > (trigger.cooldown * 1000) : true)
             .map(trigger => { return { regexp: new RegExp(trigger.trigger, "").exec(message.content), id: trigger._id, response: trigger.response } })
             .filter(trigger => trigger.regexp != null);
 
         if (triggerCanidates.length > 0) {
             let trigger = triggerCanidates[0];
 
-            // TODO: Do scripting logic.
             await processResponse(message, trigger, services);
 
-            this.lastTriggered.set(trigger.id, Date.now());
+            this.lastTriggered.set(trigger.id.toHexString(), Date.now());
             await services.database.collections.triggers.updateOne({ _id: trigger.id }, {
                 $inc: {
                     "meta.uses": 1
