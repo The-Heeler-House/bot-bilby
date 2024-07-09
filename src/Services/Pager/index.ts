9841fc49ef49035a0ff64f39ab6b8141de3f6360
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-07-09 02:02:02 +0100

    Supress #10: AbortError: This operation was aborted
    After several days of debugging, we have come to the conclusion that we're unable to patch this bug. To avoid it from constantly bugging devs whenever it triggers, This commit adds ignored errors to the Pager, which identifies an error via a 64bit hash generated by the stack trace. If an error hash matches an ignored error, it is dropped. It will still be logged to the console, however.
    
    Future errors and crashes will also show it's hash on the paged message.

--- src/Services/Pager/index.ts
+++ src/Services/Pager/index.ts
@@ -1,9 +1,50 @@
 import { Client, Snowflake, TextChannel } from "discord.js";
 import { channelIds } from "../../constants";
 
+/**
+ * Calculate a 32 bit FNV-1a hash
+ * Found here: https://gist.github.com/vaiorabbit/5657561
+ * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param {string} str the input value
+ * @param {boolean} [asString=true] set to true to return the hash value as 
+ *     8-digit hex string instead of an integer
+ * @param {integer} [seed] optionally pass the hash of the previous chunk
+ * @returns {integer | string}
+ */
+function hashFnv32a(str: string, asString: boolean = true, seed: number = undefined) {
+    /*jshint bitwise:false */
+    var i, l,
+        hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+    for (i = 0, l = str.length; i < l; i++) {
+        hval ^= str.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    if( asString ){
+        // Convert to 8 digit hex string
+        return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+    }
+    return hval >>> 0;
+}
+
+/**
+ * Hashes a stack trace, allowing for uniquly identifying this specific error.
+ * Internally calls a hashing function twice to lower chances of collisions.
+ * @param stack The stack trace to hash.
+ */
+function hashError(stack: string): string {
+    var h1 = hashFnv32a(stack) as string;  // returns 32 bit (as 8 byte hex string)
+    return h1 + hashFnv32a(h1 + stack);
+}
+
 export default class PagerService {
     protected client: Client;
     protected loggingChannel: TextChannel;
+
+    private ignoredErrorHashes: string[] = [
+        "crash-429222199c95a130", // AbortError crash from Discord.js internals that we cannot catch.
+    ];
 
     constructor(client: Client) {
         client.on("ready", async () => {
@@ -17,19 +58,30 @@ export default class PagerService {
         // This method is designed for paging a complete and total failure of Bot Bilby.
         // If this is called, Bilby has already crashed and there's nothing we can do to prevent it.
         // But before we exit, we send a "crash" page to the bilby channel to allow for debugging.
+
+        let errorHash = hashError(error.stack);
+
+        if (this.ignoredErrorHashes.includes("crash-" + errorHash)) return; // We ignore this error.
+
         await this.loggingChannel.send(`${pingList.map(userId => `<@${userId}>`).join(" ")} Crash thrown!\n\n` +
             `**Bot Bilby has crashed. Further information is available below.**\n\n` +
             `**Message:**\n\`\`\`${error.message}\`\`\`\n\n` +
             `**Stack Trace:**\n\`\`\`${error.stack}\`\`\`\n` +
-            `**Origin:**\n\`${origin}\``);
+            `**Origin:**\n\`${origin}\`\n` +
+            `**Hash:** \`${errorHash}\``);
     }
 
     public async sendError(error: Error, whileDoing: string, pingList: Snowflake[]) {
+        let errorHash = hashError(error.stack);
+
+        if (this.ignoredErrorHashes.includes("error-" + errorHash)) return; // We ignore this error.
+
         await this.loggingChannel.send(`${pingList.map(userId => `<@${userId}>`).join(" ")} Error thrown!\n\n` +
             `**Bot Bilby has encountered an error. More information is available below.**\n\n` +
             `**Message:**\n\`\`\`${error.message}\`\`\`\n\n` +
             `**Stack Trace:**\n\`\`\`${error.stack}\`\`\`\n` +
-            `**While:**\n\`${whileDoing}\``);
+            `**While:**\n\`${whileDoing}\`` +
+            `**Hash:** \`${errorHash}\``);
     }
 
     public async sendPage(...message: string[]) {
