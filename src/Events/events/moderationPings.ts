56da93e13da68d1861cbd190407b55af604564b8
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-07-26 23:54:56 +0100

    Don't ping for staff pings IN staff

--- src/Events/events/moderationPings.ts
+++ src/Events/events/moderationPings.ts
@@ -11,7 +11,10 @@ export default class ModerationPingEvent extends BotEvent {
         if (!isTHHorDevServer(message.guild.id)) return;
 
         if (message.mentions.roles.has(roleIds.staff) || message.mentions.roles.has(roleIds.mod)) {
+            if (message.channel.id == channelIds.staff) return;
+
             const staffChatChannel = await message.client.channels.fetch(channelIds.staff) as TextChannel;
+
             // Send the message link to the #staff-chat channel
             const messageLink = `https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`;
             await staffChatChannel.send(`Moderator ping detected!\n${messageLink}`);
