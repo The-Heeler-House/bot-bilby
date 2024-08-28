4932ae3f1887c4dc2cdae2c54717eb85dc95b6f4
D. Quan <60545346+sudoker0@users.noreply.github.com>
2024-08-28 22:11:50 +0700

    add ability to add suffix to duration

--- src/Commands/slash/muteme.ts
+++ src/Commands/slash/muteme.ts
@@ -6,21 +6,44 @@ export default class MuteMeCommand extends SlashCommand {
     public data = new SlashCommandBuilder()
         .setName("muteme")
         .setDescription("Is the Heeler House too addicting? I have the fix for you!")
-        .addIntegerOption(option =>
+        .addStringOption(option =>
             option.setName("length")
-                .setDescription('Length in hours that you desire to be muted for. Default is 1 hour.')
-                .setRequired(false)
-                .setMinValue(0)
-                .setMaxValue(730))
+                .setDescription('Amount you want to be muted for. Type in the duration followed by the suffix "h", "m" or "s".')
+                .setMinLength(2)
+                .setMaxLength(16)
+                .setRequired(true))
 
     async execute(interaction: ChatInputCommandInteraction, services: Services) {
-        const TIME = interaction.options.getInteger("length") ?? 1
-        const hour = TIME === 1 ? "hour" : "hours"
+        const INPUT = interaction.options.getString("length")
+        const regexResult = /^(\d+)([hms])$/g.exec(INPUT)
+        if (!regexResult) {
+            await interaction.reply({
+                ephemeral: true,
+                content: "Invalid duration specified!"
+            })
+            return
+        }
+
+        const [_, duration, suffix] = regexResult
+
+        let muteTime = 0
+        switch (suffix) {
+            case "h":
+                muteTime = Number(duration) * 3600_000
+                break
+            case "m":
+                muteTime = Number(duration) * 60_000
+                break
+            case "s":
+                muteTime = Number(duration) * 1_000
+                break
+        }
+
         if (interaction.member instanceof GuildMember) {
             const EMBED = new EmbedBuilder()
                 .setColor(0xe27a37)
                 .setTitle("Muted!")
-                .setDescription(`You have been muted for ${TIME} ${hour}! Thank you for using the Heeler House's detox service.`)
+                .setDescription(`You have been muted for ${duration}${suffix}! Thank you for using the Heeler House's detox service.`)
                 .setImage("https://c.tenor.com/Y1rAFV25rVEAAAAC/tenor.gif")
                 .setTimestamp()
             await interaction.reply({
@@ -29,7 +52,7 @@ export default class MuteMeCommand extends SlashCommand {
 
             await interaction.member
                 .timeout(
-                    TIME * 3_600_000,
+                    muteTime,
                     "Needed some time away from the server."
                 )
                 .catch(async (error: any) => {
