d2537fc36876fd671e5b04d372f312bd9017396a
D. Quan <60545346+sudoker0@users.noreply.github.com>
2024-09-03 10:37:25 +0700

    small change

--- src/Commands/slash/bingo.ts
+++ src/Commands/slash/bingo.ts
@@ -8,7 +8,6 @@ import {
 import { Services } from "../../Services"
 import SlashCommand from "../SlashCommand"
 import path from "path"
-import { readdir } from "fs/promises"
 import { CanvasRenderingContext2D, createCanvas, loadImage, registerFont } from "canvas"
 import { readFileSync } from "fs"
 
@@ -46,46 +45,48 @@ export default class BingoCommand extends SlashCommand {
         .setName("bingo")
         .setDescription("Create a Bingo card to play while watching Bluey Episodes!")
 
-    async execute(
-        interaction: ChatInputCommandInteraction<CacheType>,
-        services: Services): Promise<void>
-    {
-
-        // var ITEM_LIST = await readdir(BINGO_IMAGE_DIR)
-        // ITEM_LIST = ITEM_LIST.filter(v => v.startsWith("Prompt - "))
-        // ITEM_LIST = ITEM_LIST.sort(_ => 0.5 - Math.random()) //? shuffle
-        // ITEM_LIST = ITEM_LIST.slice(0, 25)
-        const boardSize = 5
-        const imageSize = 128
+    async execute(interaction: ChatInputCommandInteraction<CacheType>, services: Services): Promise<void> {
+        //? configurable settings
+        const boardSize = { rows: 5, cols: 5 }
         const fontSize = 16
         const textGap = 16
         const padding = 16
         const textColor = "rgb(90, 90, 135)"
-        const canvasSize = boardSize * imageSize
+
+        const cellBgImage = await loadImage(
+            path.join(bingoDataDir, bgFile))
+
+        const cellSize = {
+            w: cellBgImage.width,
+            h: cellBgImage.height
+        }
+
+        const canvasSize = {
+            w: boardSize.rows * cellSize.w,
+            h: boardSize.cols * cellSize.h
+        }
 
         let eps = epsData.split("\n")
         eps = eps.sort(_ => 0.5 - Math.random())
-        eps = eps.slice(0, boardSize ** 2)
+        eps = eps.slice(0, boardSize.rows * boardSize.cols)
 
-        const bingoCard = createCanvas(canvasSize, canvasSize)
+        const bingoCard = createCanvas(canvasSize.w, canvasSize.h)
         const ctx = bingoCard.getContext("2d")
-        const IMG = await loadImage(
-            path.join(bingoDataDir, bgFile))
 
         ctx.font = `${fontSize}px "Hello Headline"`
         ctx.fillStyle = textColor
         ctx.textAlign = "center"
         ctx.textBaseline = "middle"
 
-        for (var i = 0; i < boardSize; i++) {
-            for (var j = 0; j < boardSize; j++) {
-                ctx.drawImage(IMG, i * imageSize, j * imageSize, imageSize, imageSize)
+        for (var i = 0; i < boardSize.rows; i++) {
+            for (var j = 0; j < boardSize.cols; j++) {
+                ctx.drawImage(cellBgImage, i * cellSize.w, j * cellSize.h, cellSize.w, cellSize.h)
 
                 //? manually calculating where to put text. don't ask, it's black magic!
-                const texts = getLines(ctx, eps[i * boardSize + j], imageSize - padding * 2)
+                const texts = getLines(ctx, eps[i * boardSize.rows + j], cellSize.w - padding * 2)
                 const offset = -textGap / 2 * (texts.length - 1)
                 for (let k = 0; k < texts.length; k++) {
-                    ctx.fillText(texts[k], i * imageSize + imageSize / 2, j * imageSize + imageSize / 2 + offset + k * textGap)
+                    ctx.fillText(texts[k], i * cellSize.w + cellSize.w / 2, j * cellSize.h + cellSize.h / 2 + offset + k * textGap)
                 }
             }
         }
