92c264ba003fb3f6342011b043c812b450b1758c
Cloudburst (Leah) <43821940+CloudburstSys@users.noreply.github.com>
2024-10-23 01:37:53 +0100

    Update unignore_channel.ts

--- src/Commands/text/unignore_channel.ts
+++ src/Commands/text/unignore_channel.ts
@@ -28,7 +28,7 @@ export default class UnignoreChannelComamnd extends TextCommand {
             return;
         }
 
-        services.state.state.ignoredChannels.push(args[0]);
+        services.state.state.ignoredChannels = services.state.state.ignoredChannels.filter(item => item != args[0]);
         services.state.save();
 
         message.reply("Got it, I'll listen to the <#" + args[0] + "> channel from now on");
