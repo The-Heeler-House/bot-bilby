a54bfb7c5b418b449f50622173d448bb34c0779a
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-01-10 08:44:59 +0700

    oops

--- src/Commands/text/toggle_media.ts
+++ src/Commands/text/toggle_media.ts
@@ -28,6 +28,6 @@ export default class ToggleMediaCommand extends TextCommand {
             })
             current = !currentPerms.allow.has(PermissionsBitField.Flags.AttachFiles)
         }
-        await channel.send(`Successfully **${current ? "allow" : "disallow"}** media perms for everyone in <#${args["channel"]}> (excluding staff).`);
+        await message.reply(`Successfully **${current ? "allow" : "disallow"}** media perms for everyone in <#${args["channel"]}> (excluding staff).`);
     }
 }
