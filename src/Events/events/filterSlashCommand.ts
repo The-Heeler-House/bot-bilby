092257a9e70cbb6d8a829c68c47189f2c47204f2
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-01-27 10:26:27 +0700

    fix the filter

--- src/Events/events/filterSlashCommand.ts
+++ src/Events/events/filterSlashCommand.ts
@@ -1,7 +1,7 @@
 import { Client, Events, Interaction, TextChannel } from "discord.js";
 import BotEvent from "../BotEvent";
 import { Services } from "../../Services";
-import { badWordRegex, channelIds } from "../../constants";
+import { badWordFilter, channelIds } from "../../constants";
 
 export default class FilterSlashCommandEvent extends BotEvent {
     public eventName = Events.InteractionCreate;
@@ -12,20 +12,33 @@ export default class FilterSlashCommandEvent extends BotEvent {
 
         const concatenatedOptions = interaction.options.data.map(v => `[${v.name}: ${v.value}]`).join(" ")
 
-        for (let data of interaction.options.data) {
-            let value = String(data.value)
-            console.log(value, badWordRegex.test(value))
-            if (badWordRegex.test(value)) {
-                await interaction.deleteReply()
-                await interaction.followUp({
-                    content: `<@${interaction.user.id}>, Watch your language!`,
-                    ephemeral: true
-                })
-                await logChannel.send({
-                    content: `Interaction sent by <@${interaction.user.id}> deleted in <#${interaction.channelId}>. Interaction command was: \`/${interaction.commandName} ${concatenatedOptions}\``
-                })
-                return
+        let concatenatedData = interaction.options.data.map(v => v.value).join(" ")
+        let wordList = concatenatedData.split(/\b/g).map(v => v.trim().toLowerCase().normalize("NFD").replace(/(\p{Diacritic})|[^a-zA-Z0-9 ]/gu, "")).filter(v => v)
+        let detected = false
+        detect_loop: for (let word of wordList) {
+            for (let matchAll of badWordFilter.match_all) {
+                if (word.includes(matchAll)) {
+                    detected = true
+                    break detect_loop
+                }
             }
+            for (let matchExact of badWordFilter.match_exact) {
+                if (word == matchExact.toLowerCase()) {
+                    detected = true
+                    break detect_loop
+                }
+            }
+        }
+        if (detected) {
+            await interaction.deleteReply()
+            await interaction.followUp({
+                content: `<@${interaction.user.id}>, Watch your language!`,
+                ephemeral: true
+            })
+            await logChannel.send({
+                content: `Interaction sent by <@${interaction.user.id}> deleted in <#${interaction.channelId}>. Interaction command was: \`/${interaction.commandName} ${concatenatedOptions}\``
+            })
+            return
         }
     }
 }
