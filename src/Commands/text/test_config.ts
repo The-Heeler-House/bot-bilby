c690e654b25d7abfb5f6961dbd2fb25ca49d9254
D. Quan <60545346+sudoker0@users.noreply.github.com>
2026-09-20 18:48:38 +0700

    Update test_config.ts

--- src/Commands/text/test_config.ts
+++ src/Commands/text/test_config.ts
@@ -31,21 +31,28 @@ export default class TestConfigCommand extends TextCommand {
             channels: {} as {
                 [name: string]: {
                     id: string;
+                    cName: string;
                     canRead: boolean;
                     canSend: boolean;
                 };
             },
-            roles: {} as { [name: string]: { id: string; exists: boolean } },
+            roles: {} as {
+                [name: string]: { id: string; rName: string; exists: boolean };
+            },
         };
         for (const cName in channelIds) {
             const cId = channelIds[cName];
             result.channels[cName] = {
                 id: cId,
+                cName: "unknown",
                 canRead: false,
                 canSend: false,
             };
             try {
                 const cInstance = await message.guild.channels.fetch(cId);
+                if (cInstance) {
+                    result.channels[cName].cName = cInstance.name;
+                }
                 const me = message.guild.members.me;
                 const perms = cInstance.permissionsFor(me);
                 if (!perms) {
@@ -67,9 +74,12 @@ export default class TestConfigCommand extends TextCommand {
         }
         for (const roles in roleIds) {
             const rId = roleIds[roles];
-            result.roles[roles] = { id: rId, exists: false };
+            result.roles[roles] = { id: rId, rName: "unknown", exists: false };
             try {
-                await message.guild.roles.fetch(rId);
+                const rInstance = await message.guild.roles.fetch(rId);
+                if (rInstance) {
+                    result.roles[roles].rName = rInstance.name;
+                }
                 result.roles[roles].exists = true;
             } catch {
                 result.roles[roles].exists = false;
@@ -78,14 +88,23 @@ export default class TestConfigCommand extends TextCommand {
         const textOutput = [
             "Test finished!",
             "Channels:",
-            ...Object.entries(result.channels).map(
-                ([cName, cInfo]) =>
-                    ` \\- ${cName} (<#${cInfo.id}>) [${cInfo.id}] read=${boolToEmoji(cInfo.canRead)}, send=${boolToEmoji(cInfo.canSend)}`,
+            ...Object.entries(result.channels).map(([cName, cInfo]) =>
+                [
+                    ` \\- ${cName}`,
+                    `    name: ${cInfo.cName}`,
+                    `    id: ${cInfo.id}`,
+                    `    can_read: ${boolToEmoji(cInfo.canRead)}`,
+                    `    can_send: ${boolToEmoji(cInfo.canSend)}`,
+                ].join("\n"),
             ),
             "Roles:",
-            ...Object.entries(result.roles).map(
-                ([rName, rInfo]) =>
-                    ` \\- ${rName} [${rInfo.id}]: exists=${boolToEmoji(rInfo.exists)}`,
+            ...Object.entries(result.roles).map(([rName, rInfo]) =>
+                [
+                    ` \\- ${rName}`,
+                    `    name: ${rInfo.rName}`,
+                    `    id: ${rInfo.id}`,
+                    `    exists: ${boolToEmoji(rInfo.exists)}`,
+                ].join("\n"),
             ),
         ];
         await output.edit(textOutput.join("\n"));
