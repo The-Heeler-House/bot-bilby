49c95f395410137031a560050ea43a34ae380a0b
D. Quan <60545346+sudoker0@users.noreply.github.com>
2026-09-20 18:38:43 +0700

    Update test_config.ts

--- src/Commands/text/test_config.ts
+++ src/Commands/text/test_config.ts
@@ -8,6 +8,10 @@ import { Services } from "../../Services";
 import TextCommand, { TextCommandBuilder } from "../TextCommand";
 import { roleIds, channelIds } from "../../constants";
 
+function boolToEmoji(boolean) {
+    return boolean ? "✅" : "❌";
+}
+
 export default class TestConfigCommand extends TextCommand {
     public data = new TextCommandBuilder()
         .setName("test config")
@@ -25,55 +29,63 @@ export default class TestConfigCommand extends TextCommand {
         );
         const result = {
             channels: {} as {
-                [key: string]: { canRead: boolean; canSend: boolean };
+                [name: string]: {
+                    id: string;
+                    canRead: boolean;
+                    canSend: boolean;
+                };
             },
-            roles: {} as { [key: string]: boolean },
+            roles: {} as { [name: string]: { id: string; exists: boolean } },
         };
-        for (const channel in channelIds) {
-            result.channels[channel] = { canRead: false, canSend: false };
-            const id = channelIds[channel];
+        for (const cName in channelIds) {
+            const cId = channelIds[cName];
+            result.channels[cName] = {
+                id: cId,
+                canRead: false,
+                canSend: false,
+            };
             try {
-                const channelInstance = await message.guild.channels.fetch(id);
+                const cInstance = await message.guild.channels.fetch(cId);
                 const me = message.guild.members.me;
-                const perms = channelInstance.permissionsFor(me);
+                const perms = cInstance.permissionsFor(me);
                 if (!perms) {
-                    result.channels[channel].canRead = false;
-                    result.channels[channel].canSend = false;
+                    result.channels[cName].canRead = false;
+                    result.channels[cName].canSend = false;
                 }
-                const sendPerm = channelInstance.isThread()
+                const sendPerm = cInstance.isThread()
                     ? PermissionFlagsBits.SendMessagesInThreads
                     : PermissionFlagsBits.SendMessages;
-                result.channels[channel].canSend = perms.has([sendPerm]);
-                result.channels[channel].canRead = perms.has([
+                result.channels[cName].canSend = perms.has([sendPerm]);
+                result.channels[cName].canRead = perms.has([
                     PermissionFlagsBits.ViewChannel,
                     PermissionFlagsBits.ReadMessageHistory,
                 ]);
             } catch {
-                result.channels[channel].canRead = false;
-                result.channels[channel].canSend = false;
+                result.channels[cName].canRead = false;
+                result.channels[cName].canSend = false;
             }
         }
         for (const roles in roleIds) {
-            result.roles[roles] = false;
-            const id = roleIds[roles];
+            const rId = roleIds[roles];
+            result.roles[roles] = { id: rId, exists: false };
             try {
-                await message.guild.roles.fetch(id);
-                result.roles[roles] = true;
+                await message.guild.roles.fetch(rId);
+                result.roles[roles].exists = true;
             } catch {
-                result.roles[roles] = false;
+                result.roles[roles].exists = false;
             }
         }
         const textOutput = [
-            "Test result for server:",
+            "Test finished!",
             "Channels:",
             ...Object.entries(result.channels).map(
-                ([channel, perms]) =>
-                    ` \\- <#${channel}> (${channel}) read=${perms.canRead}, send=${perms.canSend}`,
+                ([cName, cInfo]) =>
+                    ` \\- ${cName} (<#${cInfo.id}>) [${cInfo.id}] read=${boolToEmoji(cInfo.canRead)}, send=${boolToEmoji(cInfo.canSend)}`,
             ),
             "Roles:",
             ...Object.entries(result.roles).map(
-                ([role, hasRole]) =>
-                    ` \\- <@&${role}> (${role}): ${hasRole ? "exists" : "does not exist"}`,
+                ([rName, rInfo]) =>
+                    ` \\- ${rName} [${rInfo.id}]: exists=${boolToEmoji(rInfo.exists)}`,
             ),
         ];
         await output.edit(textOutput.join("\n"));
